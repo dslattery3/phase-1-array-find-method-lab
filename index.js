cba97791188c5bb8b0944c1c68cb74d4e99c7ca8
Dalton <slattery.da@gmail.com>
2022-01-12 10:01:50 -0600

    commit tree 041009d7751b8f305e6e4ac270e86ec0f674e55b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+// // code your solution here
+// function superbowlWin(array) {
+//     let i;
+//     let wins = [];
+//     for (i = 0; i < array.length; i++){
+//         if (array[i].result === "W"){
+//             wins = [...wins, `${array[i].year}`];
+//             console.log(wins);
+//             return array[i].year;
+//         }
+//     }
+// }
+function superbowlWin(array){
+    let foundVictory = array.find(({result}) => result === "W");
+    if (foundVictory === undefined){
+        return undefined;
+    }
+    return foundVictory.year;
+}
